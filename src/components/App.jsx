bbe4c7898ec8a22163a08b17d0da55265055b332
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-01 03:23:52 +0300

    финиш?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,15 +10,13 @@ import Notiflix from 'notiflix';
 export default class App extends Component  {
   state = {
     contacts:[
-      {id: "id-1", name: "Rosie Simpson", number: "459-12-56"},
-      {id: "id-2", name: "Hermione Kline", number: "443-89-12"},
-      {id: "id-3", name: "Eden Clements", number: "645-17-79"},
-      {id: "id-4", name: "Annie Copeland", number: "227-91-26"}
     ],
     filter: '',
     
   }
 
+
+ 
   addContact = ({name, number}) => {
     
     const contact = {
@@ -87,6 +85,32 @@ export default class App extends Component  {
     
   };
   
+  componentDidMount(){
+
+  const contacts = localStorage.getItem('contacts')
+  const parsedContacts = JSON.parse(contacts)
+  if(parsedContacts){
+    this.setState({contacts: parsedContacts})
+  }
+  
+
+}
+
+  componentDidUpdate(prevPops, prevState){
+
+    if(this.state.contacts !== prevState.contacts){
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+
+    }
+
+
+
+console.log(prevState);
+console.log(this.state);
+
+  }
+
+
 render(){
   return (
     <div> 
--- src/components/Phonebook/PhonebookForm.jsx
+++ src/components/Phonebook/PhonebookForm.jsx
@@ -45,7 +45,7 @@ render(){
       id={this.nameInputId}
     type="text"
     name="name"
-    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+    // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
    required
  />
@@ -59,7 +59,7 @@ render(){
  id={this.numberInputId}
   type="tel"
   name="number"
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
 />
