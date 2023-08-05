95ac9a847972300bb0943e44a8fa57b5c50ea3b2
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-05 22:00:19 +0300

    востановление

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,104 +1,131 @@
-import React from 'react';
-import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import Searchbar from './searchbar/searchbar';
-import fetchGallary from 'Services/GallaryApi';
-import { toast } from 'react-toastify';
-import ImageGallery from './ImageGallery/ImageGallery';
-import Button from './Button/Button';
-import Loader from './Loader/Loader';
-import Modal from './Modal/Modal';
+import React, { Component } from "react";
+import Form from "./Phonebook/PhonebookForm";
+import PhoneBookList from "./PhonebookList/PhoneBookList";
+import { nanoid } from 'nanoid'
+import Filter from "./Filter/Filter";
+import Notiflix from 'notiflix';
 
 
 
-export default class App extends Component {
+export default class App extends Component  {
   state = {
-    searchQuery: '',
-    results: null,
-    error: null,
-    page: 0,
-    showLoadMore: false,
-    showLoader: false,
-    showModal: false,
-    selectedImage: null,
-    galleryRef: React.createRef(),
+    contacts:[
+    ],
+    filter: '',
+    
   }
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.searchQuery;
-    const nextQuery = this.state.searchQuery;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
 
-    if (prevQuery !== nextQuery || prevPage !== nextPage) {
-      this.setState({ showLoader: true, error: null });
-      try {
-        const data = await fetchGallary(nextQuery, nextPage);
-        const totalPage = Math.ceil(data.totalHits / 12)
-        this.setState({
-          results: [...this.state.results, ...data.hits],
-          showLoadMore: data.totalHits > 12 * nextPage
-        });
-        !data.totalHits && toast.error("No results found. Please try again!");
-        nextPage >= totalPage && toast.warning("We're sorry, but you've reached the end of search results!");
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ showLoader: false })
-      }
+ 
+  addContact = ({name, number}) => {
+    
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+          };
+  
+    const checkName = this.state.contacts.map(el=>el.name).includes(contact.name)
+    const checkNumbers = this.state.contacts.map(el=>el.number).includes(contact.number)
+    
+    if (checkName && checkNumbers) {
+      Notiflix.Report.failure( 
+        `${contact.name} and ${contact.number} is already in contacts`);
+      return
+    } else if (checkName) {
+      Notiflix.Report.failure( 
+        `${contact.name} is already in contacts`);
+      return
+    } else if (checkNumbers){
+      Notiflix.Report.failure( 
+        `${contact.number} is already in contacts`);
+      return
     }
-  }
-  onFormSubmit = searchQueryOriginal => {
-    this.setState({ searchQuery: searchQueryOriginal, page: 1, results: [] });
-  }
-  onLoadMore = () => {
-    if (this.state.results) {
-      this.setState(prevState => ({ page: prevState.page + 1 }));
-    }
-  }
-  handleKeydown = e => {
-    if (e.code === 'Escape') {
-      this.setState({ showModal: false, selectedImage: null });
-    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
   };
-  handleBackdropClick = e => {
-    if (e.target === e.currentTarget) {
-      this.setState({ showModal: false, selectedImage: null });
-    }
-  };
-  onOpenModal = imageId => {
-    const { results } = this.state;
-    const selectedImage = results.find(image => image.id === imageId);
-    this.setState({ selectedImage: selectedImage, showModal: true });
-  }; 
 
-  render() {
-    const { results, showLoadMore, showLoader, showModal, selectedImage, searchQuery } = this.state;
-    return (
-      <div className='App'>
-        <ToastContainer 
-        autoClose={5000} 
-        pauseOnHover 
-        theme="colored" />
-        <Searchbar 
-        onSubmit={this.onFormSubmit} />
-        {searchQuery === '' && <h2 >Please enter a query to search for images!</h2>}
-        {results && <ImageGallery 
-        arrayResults={results} 
-        key={results.id} 
-        onOpenModal={this.onOpenModal} />}
-        {showLoadMore && <Button 
-        handleClick={this.onLoadMore}><span>Load More</span></Button>}
-        {showLoader && <Loader />}
-        {showModal && (
-          <Modal
-            onBackdropClose={this.handleBackdropClick}
-            onKeydownClose={this.handleKeydown}
-          >
-            <img src={selectedImage.largeImageURL} alt="imageSearch" />
-          </Modal>
-        )}
-      </div>
-    );
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+
+  toggleCompleted = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.map(contact =>
+        contact.id === contactId ? { ...contact, completed: !contact.completed } : contact,
+      ),
+    }));
+  };
+
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+
+  formSubmitHandler = data =>{
+    console.log(data);
+    data.id = nanoid();
+    this.state.contacts.push(data)
   }
+
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),)
+    
+  };
+  
+  componentDidMount(){
+
+  const contacts = localStorage.getItem('contacts')
+  const parsedContacts = JSON.parse(contacts)
+  if(parsedContacts){
+    this.setState({contacts: parsedContacts})
+  }
+  
+
+}
+
+  componentDidUpdate(prevPops, prevState){
+
+    if(this.state.contacts !== prevState.contacts){
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+
+    }
+
+
+
+console.log(prevState);
+console.log(this.state);
+
+  }
+
+
+render(){
+  return (
+    <div> 
+  <h2>Phonebook</h2>
+
+    <Form onSubmit={this.addContact}></Form>
+    
+    <Filter value={this.state.filter} onChange={this.changeFilter} />
+
+    <h2>Contacts</h2>
+    
+    <PhoneBookList 
+    contacts={this.state.contacts} 
+    visibleContacts={this.getVisibleContacts()}onDeleteContacts={this.deleteContact} />
+</div>
+  );
+}
 };
--- src/components/Error/Errorinfo.jsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorInfo({ message }) {
-    return (
-      <div role="alert" >
-        <p>{message}</p>
-      </div>
-    );
-  }
